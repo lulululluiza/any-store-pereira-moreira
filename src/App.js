21ede8e1fe5ed2f8a3995ae7a484bc2bad218172
lulululluiza <luizapmoreira0@gmail.com>
2024-07-28 21:35:10 -0300

    .

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import ItemListContainer from './components/ItemListContainer';
-import NavBar from './components/NavBar/Navbar';
+import NavBar from './components/NavBar/NavBar';
 
 function App() {
   return (
