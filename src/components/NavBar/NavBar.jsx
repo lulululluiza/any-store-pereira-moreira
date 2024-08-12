f1295c230e8bae225e8f5db510b8cfc5e61501cc
Lívia Oliveira <livia_aricia@hotmail.com>
2024-08-12 18:55:52 -0300

    Atualização CSS + logo

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,5 +1,6 @@
 import "./NavBar.css"
-import logo from './logo-placeholder.png'
+// import logo from './logo-placeholder.png'
+import logo from './logo.png'
 import { Link } from 'react-router-dom'
 import CartWidget from '../CartWidget/CartWidget'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
